469668bb4f3be75a7737165771df68ca461ea00b
Daniel Corrêa <daniel.1997.df@gmail.com>
2024-03-21 08:56:06 -0300

    feat: Implementation of delete and update methods

--- app/src/main/java/com/danfcorrea/convidados/repository/GuestRepository.kt
+++ app/src/main/java/com/danfcorrea/convidados/repository/GuestRepository.kt
@@ -35,4 +35,37 @@ class GuestRepository private constructor(context: Context) {
         }
 
     }
+
+    fun update(guest: GuestModel): Boolean{
+        return try {
+            val db = guestDataBase.writableDatabase
+            val presence = if (guest.presence) 1 else 0
+
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+            true
+        } catch (_: Exception){
+            false
+        }
+    }
+
+    fun delete(id: Int): Boolean{
+        return try {
+            val db = guestDataBase.writableDatabase
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(id.toString())
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
+            true
+        } catch (_: Exception){
+            false
+        }
+    }
 }
